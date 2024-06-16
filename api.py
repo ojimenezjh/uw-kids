61b0b288adfea29701cc963e21bca5458ce79787
ojimenezjh <ojimenezjh@uoc.edu>
2024-06-16 22:36:10 +0200

    options

--- api.py
+++ api.py
@@ -31,7 +31,7 @@ def validate_question_structure(question):
         return False
     return True
 
-@app.route('/generate-questions', methods=['POST', 'OPTIONS'])
+@app.route('/generate-questions', methods=['POST'])
 def generate_question():
     data = request.json
     subject = data.get('subject')
